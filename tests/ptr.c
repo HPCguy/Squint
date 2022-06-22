8ae5d12d52d9a4c4434158bd14e031edbfc1b6aa
HPCguy <Anonymous>
2022-06-22 05:50:30 -0700

    Modify tests/ptr.c to test ptr addition and subtraction.

--- tests/ptr.c
+++ tests/ptr.c
@@ -2,7 +2,10 @@
 
 int main()
 {
+    int i;
     int *s, *e, v;
+    int *first, *last;
+    int data[10];
 
     s = (int *) 0xbebebeb0;
     e = (int *) 0xbebebeb4;
@@ -16,6 +19,17 @@ int main()
         printf("passed\n");
     else
         printf("failed, e - s = %x\n", v);
+
+    for (i = 0; i < 10; ++i) data[i] = i;
+
+    first = data; last = &data[9];
+
+    for (i = 0; i < 10; ++i) {
+       if (first[i] != *(first + i) ||
+           last[-i] != *(last - i)) {
+          printf("failed, iter %d\n", i);
+       }
+    }
 
     return 0;
 }
