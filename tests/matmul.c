5c4ca4decdc0ac2612d840cc4ea90fd185e7aef9
HPCguy <Anonymous>
2022-08-14 23:17:37 -0700

    aFix FP bug blocking compound-assign/reduction ops.

--- tests/matmul.c
+++ tests/matmul.c
@@ -10,8 +10,7 @@ int main()
       for (c=0; c<SIZE; ++c) {
          sum = 0.0;
          for (k=0; k<SIZE; ++k) {
-            sum = sum + A[r][k] * B[k][c];
-            // sum += A[r][k] * B[k][c]; compound assign has bug 
+            sum += A[r][k] * B[k][c];
          }
          C[r][c] = sum;
       }
